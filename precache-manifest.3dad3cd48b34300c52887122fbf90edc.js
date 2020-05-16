18f54bc292d38de2884bd87e8aa8ebe5b615db25
GemsGame <a.masanovets@gmail.com>
2020-05-16 17:16:20 +0300

    Updates

---
+++ precache-manifest.3dad3cd48b34300c52887122fbf90edc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9054a05d62738898d7769ca9cd0ee80",
+    "url": "/parking/index.html"
+  },
+  {
+    "revision": "c62488460e589e8743c0",
+    "url": "/parking/static/css/2.a5e59029.chunk.css"
+  },
+  {
+    "revision": "4a41f8a28a73ef588b07",
+    "url": "/parking/static/css/main.28eede0d.chunk.css"
+  },
+  {
+    "revision": "c62488460e589e8743c0",
+    "url": "/parking/static/js/2.85e30af0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/parking/static/js/2.85e30af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a41f8a28a73ef588b07",
+    "url": "/parking/static/js/main.31a5fb3f.chunk.js"
+  },
+  {
+    "revision": "2dd31caf0d26f9ca76aa",
+    "url": "/parking/static/js/runtime-main.f2b00001.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/parking/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/parking/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/parking/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/parking/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/parking/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/parking/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/parking/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/parking/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/parking/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/parking/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/parking/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/parking/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/parking/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/parking/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/parking/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
